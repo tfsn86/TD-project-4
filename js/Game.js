55d64f2391b1d78552238d1f98d407aaba3c9b6e
tfsn <tfsn86@gmail.com>
2019-04-17 15:00:16 +0200

    Game and Phrase classes are edited.
    Constructor methods and the expected methods added to the Game and Phrase class. Started working on the startGame method.

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,32 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Game.js */
+class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = ['big sun', 'perfect storm', 'shitstorm', 'zebra', 'running sushi'];
+        this.activePhrase = null; // This is the Phrase object currently in play. null as default. 
+    }
+
+    startGame() {
+        this.activePhrase = getRandomPhrase(); // activePhrase property will be set to the Phrase object returned from a call to the getRandomPhrase() method.
+        phrase.addPhraseToDisplay(this.activePhrase);
+    }
+
+    getRandomPhrase() {
+        // A random phrase is seleceted. 
+    }
+
+    handleInteraction() {
+
+    }
+
+    removeLife() {
+
+    }
+
+    checkforWin() {
+
+    }
+
+    gameOver() {
+
+    }
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,17 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Phrase.js */
+class Phrase {
+    constructor (phrase) {
+        this.phrase = phrase;
+    }
+
+    addPhraseToDisplay() {
+
+    }
+
+    checkLetter () {
+
+    }
+
+    showMatchedLetter () {
+        
+    }
+}
