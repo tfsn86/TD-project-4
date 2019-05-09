6c8cb7179e63c9149a1b7c310929d0e069f22b94
tfsn <tfsn86@gmail.com>
2019-05-09 20:51:58 +0200

    Updated Game.js
    Functions updated:
    - gameOver
    - checkforWin
    - removeLife

--- js/Game.js
+++ js/Game.js
@@ -44,18 +44,62 @@ class Game {
     }
 
     handleInteraction() {
-        
+
     }
 
+    /**
+    * Increases the value of the missed property
+    * Removes a life from the scoreboard
+    * Checks if player has remaining lives and ends game if player is out
+    */
     removeLife() {
-
+        let heartLives = document.getElementsByClassName('tries');
+        this.missed += 1;
+        
+        if (this.missed >= heartLives.length) {
+            this.gameOver(false);
+        }
+        
+        for (let i = 0; i < this.missed; i++) {
+            heartLives[i].children[0].src = "images/lostHeart.png";
+        }
     }
 
+    /**
+    * Checks for winning move
+    * @return {boolean} True if game has been won, false if game wasn't
+    won
+    */
     checkforWin() {
+        let letterLis = document.getElementsByClassName('letter');
+        let winTrack = [];
 
+        for (let i = 0; i < letterLis.length; i++) {
+            if(letterLis[i].classList.contains('hide')) {
+              winTrack.push(letterLis[i]);
+            }
+          }
+
+        if (winTrack.length === 0) {
+            this.gameOver(true);
+        }
     }
 
-    gameOver() {
-
+    /**
+    * Displays game over message
+    * @param {boolean} gameWon - Whether or not the user won the game
+    */
+    gameOver(bool) {
+        document.getElementById('overlay').style.display = '';
+        let overlayDiv = document.getElementById('overlay');
+        let gameOverMsg = document.getElementById('game-over-message');
+        
+        if (bool) {
+            overlayDiv.className = 'win';
+            gameOverMsg.textContent = 'You have won the game!';
+        } else if (this.missed >= 5) {
+            overlay.className = 'lose';
+            gameOverMsg.textContent = 'You have lost the game!';
+        }
     }
 }
--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 /**
  * Creates a new instance of the 'Game' class and add event listeners for the start button and onscreen keyboard buttons.
  */
+
+
+let game;
+
+let startGameBtn = document.getElementById('btn__reset');
+startGameBtn.addEventListener('click', function () {
+   game = new Game();
+   game.startGame();
+});
+
+let phrase = new Phrase();
+
+
