320d7f62cc7c19e67249e097a77152b9c561c633
tfsn <tfsn86@gmail.com>
2019-05-17 15:37:55 +0200

    Display phrase
    Made changes to the function that displays the phrase.

--- js/Phrase.js
+++ js/Phrase.js
@@ -4,41 +4,38 @@
 
 class Phrase {
     constructor (phrase) {
-        this.phrase = phrase.toLowerCase(); // this is the actual phrase the Phrase object is representing. This property should be set to the phrase parameter, but converted to all lower case.
+        this.phrase = phrase.toLowerCase(); // This is the actual phrase the Phrase object is representing converted to lower case
     }
 
     /**
     * Display phrase on game board
     */
    addPhraseToDisplay() {
-    const letters = this.phrase.split('');
-    const ul = document.getElementById('phrase').firstElementChild;
+        const letters = this.phrase.split('');
+        const ul = document.getElementById('phrase').firstElementChild;
 
-    letters.forEach(char => {
-      const li = document.createElement('li');
-       li.textContent = char;
-      if (char !== ' ') {
-        li.className = `hide letter ${char}`;
-      } else {
-        li.className = `hide space`;
-      }
-      ul.append(li);
-    });
-  }
+        letters.forEach(char => {
+        const li = document.createElement('li');
+        li.textContent = char;
+        if (char !== ' ') {
+            li.className = `hide letter ${char}`;
+        } else {
+            li.className = `hide space`;
+        }
+        ul.append(li);
+        }); 
+    }
 
     /**
     * Checks if passed letter is in phrase
     * @param (string) letter - Letter to check
     */
     checkLetter (letterGuess) {
-        let liPhrase = document.querySelectorAll('#phrase ul li');
-        
-        for (let i = 0; i < liPhrase.length; i++) {
-            if (liPhrase[i].textContent === letterGuess) {
-                this.showMatchedLetter(liPhrase[i]);
+            if (this.phrase.includes(letterGuess)) {
+                return true;
+            } else {
+                return false;
             }
-            
-        }
     }
 
     /**
@@ -47,26 +44,12 @@ class Phrase {
     */
     showMatchedLetter (letterGuess) {
         let liPhrase = document.querySelectorAll('#phrase ul li');
-        
+    
         liPhrase.forEach(liLetter => {
             if (liLetter.textContent === letterGuess) {
                 let li = document.querySelector('.hide.letter.' + letterGuess);   
                 li.className = `show letter ${letterGuess}`;
             }
         });
-        
     }
-        
-    
-    
 }
-
-
-// const displayKeyboard = document.getElementsByClassName('key');
-//         for (let i = 0; i < displayKeyboard.length; i++) {
-//         displayKeyboard[i].addEventListener("click", function(event) {
-//         console.log(event.target.textContent);
-//         });
-//         }
-
-
